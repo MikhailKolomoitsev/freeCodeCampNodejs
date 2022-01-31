6ff2b924b3d248458a78607274d39c169d826383
mihon.testosterone <mihon.testosterone@gmail.com>
2022-01-31 17:01:14 +0200

    body-parser

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+const bodyParser=require('body-parser')
 require('dotenv').config()
 
 const publicRoute = __dirname + '/public'
 app.use("/public", express.static(publicRoute));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`)
   next()
