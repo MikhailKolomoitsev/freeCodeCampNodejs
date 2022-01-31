0ab44c994a90ca2faec1c671b9d9df25295872a6
mihon.testosterone <mihon.testosterone@gmail.com>
2022-01-31 15:20:45 +0200

    works

--- myApp.js
+++ myApp.js
@@ -2,18 +2,21 @@ var express = require('express');
 var app = express();
 require('dotenv').config()
 
-const publicRoute=__dirname + '/public'
-
-// app.use(express.static(publicRoute));
+const publicRoute = __dirname + '/public'
 app.use("/public", express.static(publicRoute));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
 
 console.log('Hello World')
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/json', (req, res) => {
+app.get('/json', (req, res, next) => {
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     res.json({ "message": "HELLO JSON" });
     next()
@@ -50,4 +53,4 @@ app.get('/json', (req, res) => {
 
 
 
- module.exports = app;
+module.exports = app;
