606b1f0f4b0224f3809d90518d66864a6aa1e3f3
mihon.testosterone <mihon.testosterone@gmail.com>
2022-01-31 19:20:38 +0200

    has to work

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(console.log('succes'))
+  .catch(e => console.log(e.message))
 
 // let Person;
 
