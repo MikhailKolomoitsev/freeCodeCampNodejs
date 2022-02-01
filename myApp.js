58288fdb766c07c15b2247c3efd02bc529b0e804
mihon.testosterone <mihon.testosterone@gmail.com>
2022-02-01 15:14:09 +0200

    findById

--- myApp.js
+++ myApp.js
@@ -63,7 +63,13 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, data) => {
+    if (err) {
+      console.log(err.message);
+      return
+    }
+    done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
