20039199b77c8e117bae17066cdc4336a446478a
mihon.testosterone <mihon.testosterone@gmail.com>
2022-02-01 15:23:57 +0200

    findOneAndUpdate

--- myApp.js
+++ myApp.js
@@ -93,8 +93,17 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, data) => {
+    if (err) {
+      console.log(err.message);
+      return
+    }
+    done(null, data);
+  })
 };
 
 const removeById = (personId, done) => {
