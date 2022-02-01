9fe02827c47c72672e84c63edd715aba51377706
mihon.testosterone <mihon.testosterone@gmail.com>
2022-02-01 15:09:51 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -43,7 +43,7 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  Person.find(personName, (err, data) => {
+  Person.find({name:personName}, (err, data) => {
     if (err) { return }
     done(null, data);
   })
