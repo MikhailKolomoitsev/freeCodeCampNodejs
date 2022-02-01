58489d05b51cf28385bea73326f53b6358be1991
mihon.testosterone <mihon.testosterone@gmail.com>
2022-02-01 15:59:12 +0200

    queryChain

--- myApp.js
+++ myApp.js
@@ -129,8 +129,17 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: -1 })
+    .limit(2)
+    .select('-age')
+    .exec((err, data) => {
+      if (err) {
+        console.log(err.message);
+        return
+      }
+      done(null, data);
+  })
 };
 
 /** **Well Done !!**
